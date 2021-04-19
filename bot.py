5620397a1e535f007e34fdd67af703c02e44b42e
Joaquín Monteagudo Gómez <Kindos7@gmail.com>
2021-04-20 01:48:50 +0200

    Implement basic translate command

--- bot.py
+++ bot.py
@@ -1,7 +1,13 @@
+import discord
 from discord.ext import commands
 
+from deepl import DeeplApi
 from settings import AppSettings
 
+_MESSAGE_HISTORY_LIMIT: int = 100
+
+settings: AppSettings = AppSettings.from_file()
+translator: DeeplApi = DeeplApi(settings.deepl_token)
 bot: commands.Bot = commands.Bot(command_prefix='!')
 
 
@@ -12,14 +18,16 @@ async def ping(context: commands.Context) -> None:
 
 @bot.command()
 async def translate(context: commands.Context) -> None:
-	pass
-
-
-def main() -> None:
-	settings: AppSettings = AppSettings.from_file()
-
-	bot.run(settings.discord_token)
+	message: discord.Message
+	async for message in context.history(limit=_MESSAGE_HISTORY_LIMIT):
+		if message.author == bot.user or context.message == message:
+			continue
+		translated_text: str = translator.translate(message.content)
+		await context.send(translated_text)
+		break
+	else:
+		await context.send("Found no message to translate")
 
 
 if __name__ == '__main__':
-	main()
+	bot.run(settings.discord_token)
